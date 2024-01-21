cfbfbb232006ce8ef6a4fee258128b114a3dfda4
meitoma <meitoma0511@gmail.com>
2024-01-21 09:46:40 +0900

    変数名の変更

--- part.py
+++ part.py
@@ -10,7 +10,7 @@ parts={
     4:'4_EXPERIMENTAL RESULTS,CONCLUSION',
 }
 
-class member:
+class Member:
     def __init__(self,name,first_choice_num,second_choice_num):
         self.name=name
         self.first_choice_num=first_choice_num
@@ -20,35 +20,35 @@ class member:
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path) #.envファイルの読み込み
 
-member1=member(name=os.environ.get("NAME1"),first_choice_num=4,second_choice_num=1)
-member2=member(name=os.environ.get("NAME2"),first_choice_num=4,second_choice_num=None)
-member3=member(name=os.environ.get("NAME3"),first_choice_num=1,second_choice_num=2)
-member4=member(name=os.environ.get("NAME4"),first_choice_num=1,second_choice_num=None)
+member1=Member(name=os.environ.get("NAME1"),first_choice_num=4,second_choice_num=1)
+member2=Member(name=os.environ.get("NAME2"),first_choice_num=4,second_choice_num=None)
+member3=Member(name=os.environ.get("NAME3"),first_choice_num=1,second_choice_num=2)
+member4=Member(name=os.environ.get("NAME4"),first_choice_num=1,second_choice_num=None)
 
 members=[member1,member2,member3,member4]
 choices=np.array([m.first_choice_num for m in members])
 
-Undecided_part={1,2,3,4}
-while Undecided_part:
+undecided_part={1,2,3,4}
+while undecided_part:
     unsuccessful_mem=set()
     #? partの担当者決定 
     for part_num in range(1,5):
         select_mem=set(np.where(choices==part_num)[0])
-        if len(select_mem)==0 or part_num not in Undecided_part:continue
+        if len(select_mem)==0 or part_num not in undecided_part:continue
         if len(select_mem)==1:
             members[select_mem.pop()].part_num = part_num
         else:
             decide_mem=select_mem.pop()
             members[decide_mem].part_num = part_num
             unsuccessful_mem = unsuccessful_mem.union(select_mem)
-        Undecided_part.remove(part_num)
+        undecided_part.remove(part_num)
     
     #? 未決定のmemberの希望を更新
     choices=np.array([0]*4)
-    cp_Undecided_part=set(Undecided_part)
+    cp_undecided_part=set(undecided_part)
     for m in range(4):
         if m in unsuccessful_mem:
-            choices[m] = members[m].second_choice_num if members[m].second_choice_num is not None else cp_Undecided_part.pop()
+            choices[m] = members[m].second_choice_num if members[m].second_choice_num is not None else cp_undecided_part.pop()
 
 for m in members:
     print(f"{m.name}\t:{parts[m.part_num]}")
